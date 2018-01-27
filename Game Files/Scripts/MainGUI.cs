31daaa3ea6383d384387a787ed0ea931c4505923
GDJosh <josh.wong@greenduck.co.uk>
2018-01-27 21:14:04 +0000

    GUI bits + scripts

---
+++ Game Files/Scripts/MainGUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGUI : MonoBehaviour {
+
+	public static Texture2D phone;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnGUI() {
+		GUI.DrawTexture(new Rect(10, Screen.height - 410, 394, 423), Resources.Load("PhoneMsg") as Texture);
+	}
+}
