b0067e1a86c877d4a91befc181d01aaf313acc15
GDJosh <josh.wong@greenduck.co.uk>
2018-01-27 15:12:48 +0000

    Added all the Unity files so far!

---
+++ Game Files/Scripts/PoliceNearby.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoliceNearby : MonoBehaviour {
+
+	public GameObject playerCam;
+	public float siren = 0f;
+
+	// Use this for initialization
+	void Start () {
+		//playerCam.GetComponent<AudioListener>().enabled = true;
+		//playerCam.GetComponent<AudioListener>().volume = siren;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
