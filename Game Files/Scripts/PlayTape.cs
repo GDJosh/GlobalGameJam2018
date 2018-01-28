f9663780417c86cdc827621c1d5d68e741d26fa1
GDJosh <josh.wong@greenduck.co.uk>
2018-01-28 10:00:24 +0000

    Play tape!

--- Game Files/Scripts/PlayTape.cs
+++ Game Files/Scripts/PlayTape.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PlayTape : MonoBehaviour {
 
-		public static bool cassettePlaying = false;
-
-	 void OnTriggerEnter(Collider other) {
-    }
+	public AudioClip[] audioTracks = new AudioClip[3];
+	public static bool cassettePlaying = false;
+	public static float countdown = 0;
+	public static int tracksPlayed = 0;
 	
-	void Update(){
+	void FixedUpdate(){
 		if((TapeIntoDeck.tapeEngaged == true)&&(cassettePlaying == false)){
 			playCassette();
 			cassettePlaying = true;
-			Debug.Log("Cassette On!");
+			//Debug.Log("Cassette On!");
+			countdown = 30f;
 		}
+		if(countdown > 0){
+			countdown -= 0.01677f;
+		} else {
+			stopCassette();
+		}
+	}
+	
+	void OnGUI (){
+		GUI.Label(new Rect(10, 60, 200, 100), countdown + "s");	
 	}
 	
 	void playCassette(){
 		AudioSource audio = GetComponent<AudioSource>();
+		audio.clip = audioTracks[tracksPlayed];
 		audio.Play();
+		tracksPlayed++;
+	}
+	void stopCassette(){
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Stop();
 	}
 }
